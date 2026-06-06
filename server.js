da8248188dfa9aeab738d4ec35704249713c0004
affhumayra-gif <affhumayra@gmail.com>
2026-06-07 00:35:06 +0600

    Log Firebase environment variables for debugging
    Add debug logging for Firebase environment variables.

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
+console.log('=== ENVIRONMENT DEBUG ===');
+console.log('FIREBASE_PROJECT_ID:', !!process.env.FIREBASE_PROJECT_ID);
+console.log('FIREBASE_CLIENT_EMAIL:', !!process.env.FIREBASE_CLIENT_EMAIL);
+console.log('FIREBASE_PRIVATE_KEY:', !!process.env.FIREBASE_PRIVATE_KEY);
+console.log('All env vars:', Object.keys(process.env).filter(k => k.includes('FIREBASE')));
+console.log('========================');
+
+const { admin, db, auth } = require('./firebase/admin.js');
 require('dotenv').config();
 const express = require('express');
 const cors    = require('cors');
